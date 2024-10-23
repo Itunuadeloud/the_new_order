38a7f5b8c601dc01f63bda18f53ad6ecd6872255
Itunuadeloud <132605887+Itunuadeloud@users.noreply.github.com>
2024-10-23 05:30:32 +0100

    Create connect_server.py
    i created the file

---
+++ connect_server.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from generate_prp import generate_prp  # The Python script to interact with FCEA2.exe
+
+# Streamlit Web App
+st.title("Fuel Property Input for .prp File Generation")
+
+# Collect user inputs
+fuel_type = st.text_input("Enter fuel type")
+density = st.number_input("Enter density (kg/m³)")
+specific_heat = st.number_input("Enter specific heat (J/kg.K)")
+
+# Button to trigger PRP generation
+if st.button("Generate .prp file"):
+    prp_file = generate_prp(fuel_type, density, specific_heat)
+    if prp_file:
+        with open(prp_file, "rb") as file:
+            st.download_button(label="Download .prp file", data=file, file_name=prp_file)
+    else:
+        st.error("Failed to generate .prp file")
